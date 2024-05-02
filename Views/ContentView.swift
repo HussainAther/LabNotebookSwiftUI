3da639691ce09998f147dccc92e20fde8debacfa
Syed Hussain Ather <shussainather@gmail.com>
2024-05-02 16:28:02 -0400

    display list of experiments

--- Views/ContentView.swift
+++ Views/ContentView.swift
@@ -1,8 +1,30 @@
 import SwiftUI
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct ContentView: View {
+    @StateObject var viewModel = ExperimentViewModel()
+    @State private var showingAddExperiment = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                List {
+                    ForEach(viewModel.experiments) { experiment in
+                        NavigationLink(destination: ExperimentRow(experiment: experiment)) {
+                            ExperimentRow(experiment: experiment)
+                        }
+                    }
+                }
+                .navigationTitle("Lab Notebook")
+                .navigationBarItems(trailing: Button(action: {
+                    showingAddExperiment.toggle()
+                }) {
+                    Image(systemName: "plus")
+                })
+            }
+            .sheet(isPresented: $showingAddExperiment) {
+                AddExperimentView(viewModel: viewModel)
+            }
+        }
     }
 }
 
